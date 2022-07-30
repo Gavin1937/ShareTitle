86b0b0f6d4bcd4e9cb2d480f56eae42929a06f39
Gavin1937 <gyh1837771475gyh@gmail.com>
2022-07-30 01:24:23 -0700

    replace ControllerAdvice w/ RestControllerAdvice

--- src/main/java/Gavin1937/ShareTitle/Controller/RestApiExceptionHandler.java
+++ src/main/java/Gavin1937/ShareTitle/Controller/RestApiExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -18,7 +18,7 @@ import org.json.JSONObject;
 import Gavin1937.ShareTitle.Util.Utilities;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestApiExceptionHandler extends ResponseEntityExceptionHandler
 {
     
